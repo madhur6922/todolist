db8a141b0a5852a19ecd706b31c990cbd7eefedb
madhur6922 <madhursharma6922@gmail.com>
2026-03-30 22:03:13 +0530

    Add Todo class with properties and methods

---
+++ Todo.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+public class Todo {
+    private Integer id;
+    private String title;
+    private String description;
+    private boolean status;
+
+    public Todo() {}
+
+    public Todo(Integer id, String title, String description, boolean status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public boolean isStatus() { return status; }
+    public void setStatus(boolean status) { this.status = status; }
+}
